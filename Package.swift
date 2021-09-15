87d4634bd95240d8ce06f03933bc5144879a1d2a
iChirag <ichirag.lukhi@gmail.com>
2021-09-15 19:35:54 +0530

    Location issue resolved

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "ShakeBug",
-                      url: "https://www.shakebug.com/sdk/ios/ShakeBug-1.0.17.zip",
+                      url: "https://www.shakebug.com/sdk/ios/ShakeBug-1.0.18.zip",
                       checksum: "7b2e807a2fe6ade1a544c3531c1fa57862992b02f07676a6e67a60e4902916d7"),
     ]
 )
