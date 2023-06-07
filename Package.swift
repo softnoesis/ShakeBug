d8008c698446d5c22025384136f639a6efc44b9c
iChirag <ichirag.lukhi@gmail.com>
2023-06-07 10:53:36 +0530

    push new versino

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "ShakeBug",
-                      url: "https://www.shakebug.com/sdk/ios/1.0.26/ShakeBug.xcframework.zip",
-                      checksum: "50a074ae93a428b76298e6e90a40c0bc41dd936ff12622d2f8d6da1ebed6cd98"),
+                      url: "https://www.shakebug.com/sdk/ios/1.0.27/ShakeBug.xcframework.zip",
+                      checksum: "b17aa688b913ab07b2a81c0a5e821beb2cbf1bdd9e2af79b6e5ada2b42f6c976"),
     ]
 )
--- ShakeBug.podspec
+++ ShakeBug.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "ShakeBug"
-  s.version      = "1.0.26"
+  s.version      = "1.0.27"
   s.summary      = "Framework to support bug & crash reporting along with screen recording, video, analytics, add events."
 
   # This description is used to generate tags and improve search results.
